ef124243fc09477b387d0a06bb435ef4493e7d04
Parth Choudhary <parthchoudhary4372@gmail.com>
2025-12-25 08:14:58 +0530

    preparing for deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: "/WebResume/",
   plugins: [
     react(),
     tailwindcss(),
